ffcdac96a1ee6993699c275390fde0d738ea37fb
Lexi Mayfield <alexmax2742@gmail.com>
2023-02-26 22:00:39 -0500

    Attempt to fix matrix build

--- ci/build.sh
+++ ci/build.sh
@@ -2,6 +2,4 @@
 
 SCRIPT_DIR=$(dirname -- "$0")
 
-echo "BUILD_TYPE=${BUILD_TYPE}"
-
-cmake --build "${SCRIPT_DIR}/../build" --config "${BUILD_TYPE}"
+cmake --build "${SCRIPT_DIR}/../build"
--- ci/configure.sh
+++ ci/configure.sh
@@ -5,12 +5,25 @@ SCRIPT_DIR=$(dirname -- "$0")
 rm -rfv "${SCRIPT_DIR}/../build"
 
 if [[ -z $GENERATOR ]]; then
-    GENERATOR="Ninja Multi-Config"
+    GENERATOR="Ninja"
+fi
+
+if [[ -z $BUILD_TYPE ]]; then
+    BUILD_TYPE="Debug"
+fi
+
+if [[ -z $CXX_STANDARD ]]; then
+    CXX_STANDARD="20"
+fi
+
+if [[ -z $LEXIO_TEST_NONSTD ]]; then
+    LEXIO_TEST_NONSTD="OFF"
 fi
 
 cmake \
     -S "${SCRIPT_DIR}/.." \
     -B "${SCRIPT_DIR}/../build" \
     -G "${GENERATOR}" \
+    -D "CMAKE_BUILD_TYPE=${BUILD_TYPE}" \
     -D "CMAKE_CXX_STANDARD=${CXX_STANDARD}" \
     -D "LEXIO_TEST_NONSTD=${LEXIO_TEST_NONSTD}"
