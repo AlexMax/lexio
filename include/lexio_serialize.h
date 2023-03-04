866c5df40d0a8cd4bc5afa8ee17221263ae96d36
Lexi Mayfield <alexmax2742@gmail.com>
2023-03-04 17:41:05 -0500

    Add signed integer readers and writers

--- include/lexio_serialize.h
+++ include/lexio_serialize.h
@@ -38,6 +38,12 @@ inline uint8_t ReadU8(READER &buffer)
 }
 
 template <typename READER>
+inline int8_t Read8(READER &buffer)
+{
+    return static_cast<int8_t>(ReadU8(buffer));
+}
+
+template <typename READER>
 inline uint16_t ReadU16LE(READER &buffer)
 {
     uint8_t buf[sizeof(uint16_t)] = {0};
@@ -50,6 +56,12 @@ inline uint16_t ReadU16LE(READER &buffer)
 }
 
 template <typename READER>
+inline int16_t Read16LE(READER &buffer)
+{
+    return static_cast<int16_t>(ReadU16LE(buffer));
+}
+
+template <typename READER>
 inline uint32_t ReadU32LE(READER &buffer)
 {
     uint8_t buf[sizeof(uint32_t)] = {0};
@@ -59,6 +71,12 @@ inline uint32_t ReadU32LE(READER &buffer)
         throw std::runtime_error("could not read 4 bytes");
     }
     return buf[0] | (buf[1] << 8) | (buf[2] << 16) | (buf[3] << 24);
+}
+
+template <typename READER>
+inline int32_t Read32LE(READER &buffer)
+{
+    return static_cast<int32_t>(ReadU32LE(buffer));
 }
 
 template <typename READER>
@@ -76,6 +94,12 @@ inline uint64_t ReadU64LE(READER &buffer)
            (static_cast<uint64_t>(buf[6]) << 48) | (static_cast<uint64_t>(buf[7]) << 56);
 }
 
+template <typename READER>
+inline int64_t Read64LE(READER &buffer)
+{
+    return static_cast<int64_t>(ReadU64LE(buffer));
+}
+
 template <typename WRITER>
 inline void WriteU8(WRITER &buffer, const uint8_t value)
 {
@@ -85,6 +109,12 @@ inline void WriteU8(WRITER &buffer, const uint8_t value)
     {
         throw std::runtime_error("could not write 1 byte");
     }
+}
+
+template <typename WRITER>
+inline void Write8(WRITER &buffer, const int8_t value)
+{
+    WriteU8(buffer, static_cast<uint8_t>(value));
 }
 
 template <typename WRITER>
@@ -102,6 +132,12 @@ inline void WriteU16LE(WRITER &buffer, const uint16_t value)
 }
 
 template <typename WRITER>
+inline void Write16LE(WRITER &buffer, const int16_t value)
+{
+    WriteU16LE(buffer, static_cast<uint16_t>(value));
+}
+
+template <typename WRITER>
 inline void WriteU32LE(WRITER &buffer, const uint32_t value)
 {
     uint8_t buf[sizeof(uint32_t)] = {
@@ -115,6 +151,12 @@ inline void WriteU32LE(WRITER &buffer, const uint32_t value)
     {
         throw std::runtime_error("could not write 4 bytes");
     }
+}
+
+template <typename WRITER>
+inline void Write32LE(WRITER &buffer, const int32_t value)
+{
+    WriteU32LE(buffer, static_cast<uint32_t>(value));
 }
 
 template <typename WRITER>
@@ -135,6 +177,12 @@ inline void WriteU64LE(WRITER &buffer, const uint64_t value)
     {
         throw std::runtime_error("could not write 8 bytes");
     }
+}
+
+template <typename WRITER>
+inline void Write64LE(WRITER &buffer, const int64_t value)
+{
+    WriteU64LE(buffer, static_cast<uint64_t>(value));
 }
 
 } // namespace LexIO
--- test/test_serialize.cpp
+++ test/test_serialize.cpp
@@ -55,7 +55,23 @@ TEST_CASE("Write UInt16LE", "[serialize]")
     REQUIRE(LexIO::ReadU8(buffer) == 0x99);
 }
 
-TEST_CASE("Read UInt32", "[serialize]")
+TEST_CASE("Read 16LE", "[serialize]")
+{
+    VectorBuffer buffer({0x88, 0x99});
+    REQUIRE(LexIO::Read16LE(buffer) == -26232);
+}
+
+TEST_CASE("Write Int16LE", "[serialize]")
+{
+    VectorBuffer buffer;
+    LexIO::Write16LE(buffer, -26232);
+
+    LexIO::Seek(buffer, LexIO::WhenceStart(0));
+    REQUIRE(LexIO::ReadU8(buffer) == 0x88);
+    REQUIRE(LexIO::ReadU8(buffer) == 0x99);
+}
+
+TEST_CASE("Read UInt32LE", "[serialize]")
 {
     VectorBuffer buffer({0x88, 0x99, 0xaa, 0xbb});
     REQUIRE(LexIO::ReadU32LE(buffer) == 0xbbaa9988);
@@ -73,7 +89,25 @@ TEST_CASE("Write UInt32LE", "[serialize]")
     REQUIRE(LexIO::ReadU8(buffer) == 0xbb);
 }
 
-TEST_CASE("Read UInt64", "[serialize]")
+TEST_CASE("Read Int32LE", "[serialize]")
+{
+    VectorBuffer buffer({0x88, 0x99, 0xaa, 0xbb});
+    REQUIRE(LexIO::Read32LE(buffer) == -1146447480);
+}
+
+TEST_CASE("Write Int32LE", "[serialize]")
+{
+    VectorBuffer buffer;
+    LexIO::Write32LE(buffer, -1146447480);
+
+    LexIO::Seek(buffer, LexIO::WhenceStart(0));
+    REQUIRE(LexIO::ReadU8(buffer) == 0x88);
+    REQUIRE(LexIO::ReadU8(buffer) == 0x99);
+    REQUIRE(LexIO::ReadU8(buffer) == 0xaa);
+    REQUIRE(LexIO::ReadU8(buffer) == 0xbb);
+}
+
+TEST_CASE("Read UInt64LE", "[serialize]")
 {
     VectorBuffer buffer({0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff});
     REQUIRE(LexIO::ReadU64LE(buffer) == 0xffeeddccbbaa9988);
@@ -83,6 +117,28 @@ TEST_CASE("Write UInt64LE", "[serialize]")
 {
     VectorBuffer buffer;
     LexIO::WriteU64LE(buffer, 0xffeeddccbbaa9988);
+    LexIO::Seek(buffer, LexIO::WhenceStart(0));
+
+    REQUIRE(LexIO::ReadU8(buffer) == 0x88);
+    REQUIRE(LexIO::ReadU8(buffer) == 0x99);
+    REQUIRE(LexIO::ReadU8(buffer) == 0xaa);
+    REQUIRE(LexIO::ReadU8(buffer) == 0xbb);
+    REQUIRE(LexIO::ReadU8(buffer) == 0xcc);
+    REQUIRE(LexIO::ReadU8(buffer) == 0xdd);
+    REQUIRE(LexIO::ReadU8(buffer) == 0xee);
+    REQUIRE(LexIO::ReadU8(buffer) == 0xff);
+}
+
+TEST_CASE("Read Int64LE", "[serialize]")
+{
+    VectorBuffer buffer({0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff});
+    REQUIRE(LexIO::Read64LE(buffer) == -4822678189205112);
+}
+
+TEST_CASE("Write Int64LE", "[serialize]")
+{
+    VectorBuffer buffer;
+    LexIO::Write64LE(buffer, -4822678189205112);
     LexIO::Seek(buffer, LexIO::WhenceStart(0));
 
     REQUIRE(LexIO::ReadU8(buffer) == 0x88);
