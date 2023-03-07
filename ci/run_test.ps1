cd4713ff0d7acbc2ed70a9562e0069d6d2ef34b3
Lexi Mayfield <alexmax2742@gmail.com>
2023-03-07 08:55:11 -0500

    Add script for running windows tests

---
+++ ci/run_test.ps1
@@ -0,0 +1,13 @@
+$BUILD_TYPE=${Env:BUILD_TYPE}
+
+if (-Not $BUILD_TYPE) {
+    $BUILD_TYPE="Debug"
+}
+
+Write-Output "CMAKE_BUILD_TYPE=${BUILD_TYPE}"
+
+Push-Location "${PSScriptRoot}/../build"
+
+& "./${BUILD_TYPE}/test.exe"
+
+Pop-Location
