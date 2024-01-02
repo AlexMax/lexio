e8b2eae2ca0d4c57de1edd2f2d05375ca45c383a
Lexi Mayfield <alexmax2742@gmail.com>
2024-01-01 20:01:30 -0500

    Try not using vswhere during build

--- ci/build.ps1
+++ ci/build.ps1
@@ -1,7 +1,12 @@
 $BUILD_TYPE="${Env:BUILD_TYPE}"
+$USE_VSWHERE="${Env:USE_VSWHERE}"
 
 if (-Not $BUILD_TYPE) {
     $BUILD_TYPE="Debug"
+}
+
+if (-Not $USE_VSWHERE) {
+    $USE_VSWHERE=""
 }
 
 Write-Output "CMAKE_BUILD_TYPE=${BUILD_TYPE}"
@@ -9,9 +14,11 @@ Write-Output "CMAKE_BUILD_TYPE=${BUILD_TYPE}"
 Push-Location .
 
 # Find Visual Studio and set up compile environment.
-$VSWHERE = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-$VSPATH = & "${VSWHERE}" -latest -property installationPath
-& "${VSPATH}\Common7\Tools\Launch-VsDevShell.ps1"
+if($USE_VSWHERE) {
+    $VSWHERE = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+    $VSPATH = & "${VSWHERE}" -latest -property installationPath
+    & "${VSPATH}\Common7\Tools\Launch-VsDevShell.ps1"
+}
 
 # Build
 cmake --build "${PSScriptRoot}\..\build" --config "${BUILD_TYPE}"
